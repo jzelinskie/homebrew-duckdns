4a4f4bf0a1ee1e5fb1614aa64f62461431c83509
Jimmy Zelinskie <jimmy.zelinskie+git@gmail.com>
2016-08-01 12:51:04 -0400

    replace sha1 checksum with sha256 checksum
    Fixes #2.

--- duckdns.rb
+++ duckdns.rb
@@ -3,7 +3,7 @@ require "formula"
 class Duckdns < Formula
   homepage "http://jzelinskie.com/homebrew-duckdns"
   url "https://github.com/jzelinskie/homebrew-duckdns/archive/1.0.tar.gz"
-  sha1 "e44316847fcacb88995a39769d1ac5bdaad1bc9a"
+  sha256 "87108c4bafb4d5b9c66dfb3cd3efa9f0cb152981ef40d763a11230247d48484c"
 
   def install
     bin.install 'duckdns'
